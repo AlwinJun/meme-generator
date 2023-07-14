2818c5add0da02bc5c8965f7b52b434592ea96b4
AJ <mahidlawonaljun@gmail.com>
2023-07-14 21:22:02 +0800

    Render meme image with useState hooks

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+import memesData from '../memesData';
 import './meme.css';
 
 const Meme = () => {
+  const [image, setImage] = useState();
+
+  const getImage = (e) => {
+    e.preventDefault();
+    setImage(() => {
+      let memeUrl;
+      const memes = memesData.data.memes;
+      const random = Math.floor(Math.random() * memes.length);
+      return (memeUrl = memes[random].url);
+    });
+  };
+
   return (
     <main>
       <form className="form">
         <input type="text" placeholder="Top text" className="form__input" />
         <input type="text" placeholder="Bottom text" className="form__input" />
-        <button className="form__button">Get a new meme image 🖼</button>
+        <button className="form__button" onClick={getImage}>
+          Get a new meme image 🖼
+        </button>
       </form>
+      {image && <img src={image} className="meme-image" alt="memes" />}
     </main>
   );
 };
