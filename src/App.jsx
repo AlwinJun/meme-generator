83ba2f076dc8aa437661efa123c2be43346ee994
AJ <mahidlawonaljun@gmail.com>
2023-07-14 21:22:16 +0800

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Header from './components/Header';
+import Meme from './components/Meme';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Test</h1>
+      <Header />
+      <Meme />
     </>
   );
 }
